6b61f5d458a2dba2bd96e23ce8f6224de47066e6
sophie cuthbertson <scuthbertson19@VenerableBede.schools.sunderland.gov.uk>
2016-09-19 14:26:59 +0100

    Finished Bobs Change

--- bobs_change.py
+++ bobs_change.py
@@ -34,4 +34,11 @@ def run_tests():
 # Edit this code:
 def bobs_change(money):
 
+    if money < 50:
+       print("Sorry Bob, you don't have enough money")
+
+    elif money > 50:
+        print(money - 50)
+        
+
 
